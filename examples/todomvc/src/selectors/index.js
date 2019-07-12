896f5725cf544cefdc6ad6d8db9b9b2d710b717e
高橋　勇人 <hayato_takahashi@r.recruit.co.jp>
2019-07-12 16:16:03 +0900

    Implement todo sort

--- examples/todomvc/src/selectors/index.js
+++ examples/todomvc/src/selectors/index.js
@@ -1,19 +1,28 @@
 import { createSelector } from 'reselect'
 import { SHOW_ALL, SHOW_COMPLETED, SHOW_ACTIVE } from '../constants/TodoFilters'
+import { ASCENT_ORDER } from  '../constants/TodoOrders'
 
 const getVisibilityFilter = state => state.visibilityFilter
+const getTaskOrder = state => state.taskOrder
 const getTodos = state => state.todos
 
 export const getVisibleTodos = createSelector(
-  [getVisibilityFilter, getTodos],
-  (visibilityFilter, todos) => {
+  [getVisibilityFilter, getTaskOrder, getTodos],
+  (visibilityFilter, taskOrder, todos) => {
+    const sortTodos = (todos, order) => {
+      return todos.sort((one, another) => {
+        const direction = order === ASCENT_ORDER ? 1 : -1
+        return direction * (new Date(one.date) - new Date(another.date))
+      })
+    }
+
     switch (visibilityFilter) {
       case SHOW_ALL:
-        return todos
+        return sortTodos(todos, taskOrder).slice()
       case SHOW_COMPLETED:
-        return todos.filter(t => t.completed)
+        return sortTodos(todos.filter(t => t.completed), taskOrder).slice()
       case SHOW_ACTIVE:
-        return todos.filter(t => !t.completed)
+        return sortTodos(todos.filter(t => !t.completed), taskOrder).slice()
       default:
         throw new Error('Unknown filter: ' + visibilityFilter)
     }
